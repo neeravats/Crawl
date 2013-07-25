2e5118862841117e512b5dcaa35a9bc8711b241f
neeravats <neeravats@gmail.com>
2013-07-25 15:22:04 -0700

    Update stage.sh

--- stage.sh
+++ stage.sh
@@ -6,4 +6,4 @@ SEEDFILE=$3
 
 mkdir -p ${DESTDIR}
 hadoop fs -copyToLocal ${LIBBUCKET}/apache-nutch-1.7.job  ${DESTDIR}/apache-nutch-1.7.job
-hadoop fs -copyToLocal ${LIBBUCKET}/${SEED_FILE}  ${DESTDIR}/apache-nutch-1.7.job
+hadoop fs -copyToLocal ${LIBBUCKET}/${SEED_FILE}  ${DESTDIR}/${SEED_FILE}
